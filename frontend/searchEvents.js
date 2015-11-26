74f1e623a407df52a4a95303c0e59be6f5f01a77
Eric Romrell <eric_romrell@byu.edu>
2015-11-26 12:36:06 -0800

    Finished search events page

--- frontend/searchEvents.js
+++ frontend/searchEvents.js
@@ -1,19 +1,53 @@
 /**
  * Created by eric on 11/26/15.
  */
-$(function() {
-	var events = [];
-	$.getJSON("http://cs256-websolutions.com/ath-meet/backend/allEvents.json", function(data) {
+var events = [];
 
+$(function() {
+	var textBox = $("#tags");
+	$.getJSON("http://cs256-websolutions.com/ath-meet/backend/myEvents.json", function(data) {
+		textBox.attr("disabled", false).attr("placeholder", "Searching...");
+		textBox.keyup();
+		$.each(data, function(key, event) {
+			events.push(event);
+		});
 	});
 
-	$("#tags".on('input', function() {
-		var button = $("#addSport");
-		if (sportNamesArray.indexOf(textBox.val()) == -1) {
-			button.attr("disabled", true);
-		} else {
-			button.attr("disabled", false);
-			button.focus();
+	textBox.keyup(loadRows);
+});
+
+var loadRows = function() {
+	$("#eventsTable").find("tbody").html("");
+	$.each(events, function(key, event) {
+		if (searchEventForSubstring(event, $("#tags").val())) {
+			addToTable(event);
 		}
 	});
-});
+};
+
+var searchEventForSubstring = function(event, substring) {
+	if (event['date'].indexOf(substring) != -1) {
+		return true;
+	}
+	if (event['activity'].indexOf(substring) != -1) {
+		return true;
+	}
+	$.each(event['participants'], function(key, person) {
+		if (person.indexOf(substring)) {
+			return true;
+		}
+	});
+
+	return false;
+};
+
+var addToTable = function(event) {
+	var tr = $("<tr></tr>").attr("id", event['id']);
+	tr.append($("<td></td>").append(event["date"]));
+	tr.append($("<td></td>").append(event["time"]));
+	tr.append($("<td></td>").append(event["activity"]));
+	tr.click(function() {
+		console.log(tr.attr("id"));
+	});
+	$("#eventsTable").append(tr);
+};
