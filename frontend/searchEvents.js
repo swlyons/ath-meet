b489e224694204bbbff9ff14a834c1473b302532
Eric Romrell <eric_romrell@byu.edu>
2015-11-26 09:11:42 -0800

    Started search events

--- frontend/searchEvents.js
+++ frontend/searchEvents.js
@@ -1,3 +1,19 @@
 /**
  * Created by eric on 11/26/15.
  */
+$(function() {
+	var events = [];
+	$.getJSON("http://cs256-websolutions.com/ath-meet/backend/allEvents.json", function(data) {
+
+	});
+
+	$("#tags".on('input', function() {
+		var button = $("#addSport");
+		if (sportNamesArray.indexOf(textBox.val()) == -1) {
+			button.attr("disabled", true);
+		} else {
+			button.attr("disabled", false);
+			button.focus();
+		}
+	});
+});
