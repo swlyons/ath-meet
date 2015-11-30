6a9ae8fbcda5d27b5cecd82d525fbb9ac9a2d16c
Eric Romrell <eric_romrell@byu.edu>
2015-11-30 11:38:10 -0700

    Added user profile image functionality and background image

--- frontend/newsFeed.js
+++ frontend/newsFeed.js
@@ -2,23 +2,35 @@
  * Created by eric on 11/19/15.
  */
 $(function() {
-	$.getJSON("http://cs256-websolutions.com/ath-meet/backend/newsFeed.json", function(data) {
-		var table = $("<table></table>").attr("id", "feedContent").attr("cellspacing", "10");
+	$.getJSON("http://cs256-websolutions.com/ath-meet/backend/newsFeed.json", function(newsFeedData) {
+		$.getJSON("http://cs256-websolutions.com/ath-meet/backend/users.json", function(usersData) {
+			var userImageMap = transformUserData(usersData);
 
-		$.each(data, function(key, val) {
-			var row = $("<tr></tr>");
-			var cell = $("<td></td>").attr("class", "feedCell");
-			var image = $("<img />").attr("class", "profilePicture").attr("src", val["image"]);
-			var link = $("<a></a>").attr("href", "profile.html?id=" + val["username"]);
-			var imageLink = link.clone();
-			imageLink.append(image);
-			link.append("@" + val["username"]);
-			var label = $("<label></label>").append(link).append(": " + val["text"]);
-			cell.append(imageLink).append(label);
-			row.append(cell);
-			table.append(row);
+			var table = $("<table></table>").attr("id", "feedContent").attr("cellspacing", "10");
+
+			$.each(newsFeedData, function(key, feedItem) {
+				var row = $("<tr></tr>");
+				var cell = $("<td></td>").attr("class", "feedCell");
+				var image = $("<img />").attr("class", "profilePicture").attr("src", userImageMap[feedItem["username"]]);
+				var link = $("<a></a>").attr("href", "profile.html?id=" + feedItem["username"]);
+				var imageLink = link.clone();
+				imageLink.append(image);
+				link.append("@" + feedItem["username"]);
+				var label = $("<label></label>").append(link).append(": " + feedItem["text"]);
+				cell.append(imageLink).append(label);
+				row.append(cell);
+				table.append(row);
+			});
+
+			$("#mainContent").append(table);
 		});
-
-		$("#mainContent").append(table);
 	});
 });
+
+var transformUserData = function(data) {
+	var userImageMap = {};
+	$.each(data, function(key, user) {
+		userImageMap[user['username']] = user['image'];
+	});
+	return userImageMap;
+};
