c751ba41e6e127766e01d7868ee35f54abc99f6b
Eric Romrell <eric_romrell@byu.edu>
2015-12-01 15:32:12 -0700

    Remove querystring stuff

--- frontend/eventDetails.js
+++ frontend/eventDetails.js
@@ -23,10 +23,3 @@ $(function() {
 		$("#details").append(event['details']);
 	});
 });
-
-function getParameterByName(name) {
-	name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
-	var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
-		results = regex.exec(location.search);
-	return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
-}
--- frontend/template.js
+++ frontend/template.js
@@ -30,3 +30,10 @@ initializeClickFunctions = function() {
 		window.location.href = "createEvent.html";
 	});
 };
+
+function getParameterByName(name) {
+	name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+	var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+		results = regex.exec(location.search);
+	return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+}
